c305a5f9ab72890f9a63dde0a4a8d43e5dafce86
shradha-deloitte <101543528+shradha-deloitte@users.noreply.github.com>
2023-07-14 11:59:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const teamRoutes = require('./routes/teamRoutes.js');
+const matchRoutes = require('./routes/matchRoutes.js');
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("hello there");
+})
+
+app.use("/", matchRoutes);
+app.use("/", teamRoutes);
+
+app.listen(5000, console.log("server has started"));
